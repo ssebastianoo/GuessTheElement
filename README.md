e2d06139df237ae19cdde9d5d0db11bff239b482
ssebastianoo <sebastianogirotto05@gmail.com>
2022-04-04 18:49:49 +0200

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# guess the element
-learn the periodic table by guessing the element based on its symbol
-
-[play](https://ssebastianoo.github.io/GuessTheElement)
--- src/stores.js
+++
@@ -1,9 +0,0 @@
-import { writable } from 'svelte/store';
-import * as json from './elements.json';
-
-export const elements = writable(json.elements);
-export const correctPoints = writable(0);
-export const totalPoints = writable(0);
-export const status = writable({guessed: null, message: ''});
-export const mode = writable("guessSymbol");
-export const guessedElements = writable([]); // [{name: "Seaborgium", symbol: "sg", number: "106", atomic_mass: "269", category: "transition metals"}]
